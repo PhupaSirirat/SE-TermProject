3ebcbf33639a6f9000a8058e091eb6cf9c17f134
KanbH <pohbnak@gmail.com>
2023-11-08 15:07:43 +0700

    Register page
    -Added a Register page.
    -Created the Register page.
    -Integrated the Register page into the router.
    -Made changes to the InputForm component.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Login from './Login';
+import Register from './Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
