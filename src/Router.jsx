dcdc240e3df56645bd9f4551ad57c01515215c50
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-11-08 13:22:47 +0700

    Refactor code

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from './components/Login';
+import Login from './Login';
 
 const router = createBrowserRouter([
   {
