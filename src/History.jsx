aad94b252163365da1a675a3776bea42278a2a32
KanbH <pohbnak@gmail.com>
2023-11-12 17:57:58 +0700

    Fix history page title and layout
    -Fix history page name from "เส้นทางที่บันทึก" to "ประวัติเดินทาง"
    -Change page to uniform layout

--- src/History.jsx
+++ src/History.jsx
@@ -3,23 +3,26 @@ import { Button } from "./components/Button";
 
 const HistoryPage = tv({
   slots: {
-    base: "flex flex-col h-screen items-center pt-20",
-    headerText: "text-3xl font-bold mb-10",
+    base: "flex flex-col h-screen items-center pt-10 bg-[#fee4c8]",
+    headerText: "text-4xl font-bold mb-10",
   },
 });
 
 const { base, headerText } = HistoryPage();
+const placeholderroute = [
+  { start: 12, destination: 34 },
+  { start: 56, destination: 78 }
+];
+
 
 export default function History() {
   return (
     <main className={base()}>
-      <h1 className={headerText()}>เส้นทางที่บันทึก</h1>
+      <h1 className={headerText()}>ประวัติเดินทาง</h1>
       <section className="flex justify-between items-center w-4/5 space-x-4">
-        <span className="text-lg font-semibold">นี่คือเส้นทาง</span>
-        <div className="flex items-center gap-3">
-            <p>5บาท</p>
-            <Button label={"บันทึก"} className={"px-2 py-1"} />
-        </div>
+
+
+
       </section>
     </main>
   );
