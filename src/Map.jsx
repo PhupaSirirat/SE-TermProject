d0125ffbc4e2c54c996a05ea3b31ff6382bdd47a
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-11-13 23:56:55 +0700

    Feat/bus stop (#30)
    * Update Map.jsx
    
    * style: map page
    
    * Create .env.example

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,9 +1,10 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   useJsApiLoader,
   GoogleMap,
   Autocomplete,
   DirectionsRenderer,
+  TransitLayer,
 } from "@react-google-maps/api";
 import { Button } from "./components/Button";
 import { Link } from "react-router-dom";
@@ -11,13 +12,13 @@ import { tv } from "tailwind-variants";
 
 const MapPage = tv({
   slots: {
-    base: "relative h-screen overflow-hidden",
-    input: "w-40 rounded-md p-0.5 pl-1.5",
+    base: "relative flex flex-col",
+    input: "w-full rounded-md p-0.5 pl-1.5",
     newbutton: "border-2 border-black rounded-md p-0.5",
   },
 });
 
-const { base, input, newbutton } = MapPage(); 
+const { base, input, newbutton } = MapPage();
 
 const center = {
   lat: 13.739415037890979,
@@ -35,27 +36,70 @@ export default function Map() {
 
   const [, setMap] = useState(null);
   const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [busStops, setBusStops] = useState([]);
 
   const originRef = useRef();
   const destiantionRef = useRef();
+  const transitLayerRef = useRef();
+
+  const onMapLoad = React.useCallback((map) => {
+    setMap(map);
+    transitLayerRef.current = new window.google.maps.TransitLayer();
+    transitLayerRef.current.setMap(map);
+  }, []);
 
   async function calculateRoute() {
-    if (originRef.current.value === "" || destiantionRef.current.value === "") {
-      return;
+    setBusStops([]);
+    try {
+      if (
+        originRef.current.value === "" ||
+        destiantionRef.current.value === ""
+      ) {
+        return;
+      }
+      const directionsService = new window.google.maps.DirectionsService();
+      const results = await directionsService.route({
+        origin: originRef.current.value,
+        destination: destiantionRef.current.value,
+        travelMode: window.google.maps.TravelMode.TRANSIT,
+      });
+
+      setDirectionsResponse(results);
+
+      // Extracting bus stops from the response
+      // let busStops = [];
+      results.routes[0].legs.forEach((leg) => {
+        leg.steps.forEach((step) => {
+          if (
+            step.travel_mode === "TRANSIT" &&
+            step.transit.line.vehicle.type === "BUS"
+          ) {
+            // Push the departure stop's name to the busStops array
+            busStops.push(step.transit.departure_stop.name);
+            // Since the arrival stop of the last step will be the departure stop of the next transit step, we don't push it here to avoid duplicates
+            // Only push the arrival stop for the last transit step in the leg
+            if (
+              leg.steps.indexOf(step) === leg.steps.length - 1 ||
+              leg.steps[leg.steps.indexOf(step) + 1].travel_mode !== "TRANSIT"
+            ) {
+              busStops.push(step.transit.arrival_stop.name);
+            }
+          }
+        });
+      });
+
+      console.log(busStops);
+      setBusStops(busStops);
+    } catch (err) {
+      alert(err);
     }
-    const directionsService = new window.google.maps.DirectionsService();
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destiantionRef.current.value,
-      travelMode: window.google.maps.TravelMode.DRIVING,
-    });
-    setDirectionsResponse(results);
   }
 
   function clearRoute() {
     setDirectionsResponse(null);
     originRef.current.value = "";
     destiantionRef.current.value = "";
+    setBusStops([]);
   }
 
   return isLoaded ? (
@@ -63,16 +107,28 @@ export default function Map() {
       <Link to="/home" className="absolute z-10 top-5 left-5">
         <Button label={"กลับ"} className={"px-2 py-1"} />
       </Link>
-      <section className="w-1/2 h-fit bg-white absolute left-1/2 -translate-x-1/2 top-5 z-10 rounded-lg border border-black grid items-center p-4">
+      {busStops.length > 0 && (
+        <div className="absolute z-10 bottom-7 bg-white w-full border-4 p-2">
+          <section>
+            <span>Routes ({busStops.length})</span>
+          </section>
+          <section>
+            {busStops.map((item) => (
+              <div key={item}>{busStops.indexOf(item)+1}.{item}</div>
+            ))}
+          </section>
+        </div>
+      )}
+      <section className="w-3/5 h-fit bg-white absolute left-1/2 -translate-x-1/2 top-5 z-10 rounded-lg border border-black grid items-center p-4">
         <div className="flex flex-col">
-          <section className="flex flex-col justify-center gap-4">
+          <section className="flex flex-col justify-center gap-4 text-white">
             <div className="">
               <Autocomplete>
                 <input
                   type="text"
-                  name="Origin"
+                  name="From"
                   className={input()}
-                  placeholder="Origin"
+                  placeholder="From"
                   ref={originRef}
                 />
               </Autocomplete>
@@ -81,9 +137,9 @@ export default function Map() {
               <Autocomplete>
                 <input
                   type="text"
-                  name="Destication"
+                  name="To"
                   className={input()}
-                  placeholder="Destication"
+                  placeholder="To"
                   ref={destiantionRef}
                 />
               </Autocomplete>
@@ -124,11 +180,12 @@ export default function Map() {
             mapTypeControl: false,
             fullscreenControl: false,
           }}
-          onLoad={(map) => setMap(map)}
+          onLoad={onMapLoad}
         >
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
+          <TransitLayer onLoad={(tl) => (transitLayerRef.current = tl)} />
         </GoogleMap>
       </section>
     </main>
